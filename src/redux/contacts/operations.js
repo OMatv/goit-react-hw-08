e3db00e074ad37d345f6c150344e6e5b08e0dd57
OMatv <149325870+OMatv@users.noreply.github.com>
2024-08-20 20:48:21 +0300

    fixed remark

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,13 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://connections-api.goit.global/";
+const contactsApi = axios.create({
+  baseURL: "https://connections-api.goit.global/",
+});
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const res = await axios.get("/contacts");
+      const res = await contactsApi.get("/contacts");
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,7 +21,7 @@ export const addContact = createAsyncThunk(
   "contacts/addContact",
   async (contact, thunkAPI) => {
     try {
-      const response = await axios.post("/contacts", contact);
+      const response = await contactsApi.post("/contacts", contact);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -31,7 +33,7 @@ export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
   async (contactId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/contacts/${contactId}`);
+      const response = await contactsApi.delete(`/contacts/${contactId}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,3 +1,16 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectAllContacts = (state) => state.contacts.items;
 export const selectLoading = (state) => state.contacts.isLoading;
 export const selectError = (state) => state.contacts.error;
+export const selectFilter = (state) => state.filters.name;
+
+export const selectFilteredContacts = createSelector(
+  [selectAllContacts, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
